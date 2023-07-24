c96f03ce6d6cfe9773c28be09254a2b97a5b7ace
Charlie Bowen <charlie.bowenn@outlook.com>
2023-07-24 16:15:45 +0100

    Basic game functionality

--- backend.py
+++ backend.py
@@ -1,5 +1,59 @@
-guess = input("Guess the Song!")
-if guess == "Despacito":
-    print("Winner winner chicken dinner")
-else:
-    print("Unlucky")
+import time
+import simpleaudio
+import os
+from pydub import AudioSegment
+from pydub.playback import play, _play_with_simpleaudio
+
+print("Heardle!")
+print("Enter a song name as a guess.\nEnter blank space to hear again.")
+roundTimes = {
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 5,
+    5: 10,
+    6: 15
+}
+song = AudioSegment.from_mp3('Songs/HotelCalifornia.mp3')
+songname = "Hotel California"
+
+
+def game():
+    counter = 1
+    correct = False
+    while counter<7 and correct!=True:
+        print(f"Round {counter}.\nYou have {roundTimes[counter]} seconds.")
+        guess = round(counter)
+        if guess.upper()==songname.upper():
+            play(AudioSegment.from_mp3('Sounds/CorrectAnswer.mp3'))
+            correct = True
+        else:
+            play(AudioSegment.from_mp3('Sounds/WrongAnswer.mp3'))
+            counter+=1
+    if correct==True:
+        print(f"Congratulations! Song guessed in {counter} rounds.")
+        play(AudioSegment.from_mp3('Sounds/GameWon.mp3'))
+    else:
+        print(f"Unlucky! The song was {songname}.")
+        play(AudioSegment.from_mp3('Sounds/GameLost.mp3'))
+
+
+def round(num):
+    guessed = False
+    while guessed != True:
+        playSong(num)
+        guess = input("Guess: ")
+        if guess != "":
+            guessed=True
+        else:
+            print("Playing again...")
+    return guess
+
+
+def playSong(num):
+    playback = _play_with_simpleaudio(song)
+    time.sleep(roundTimes[num])
+    playback.stop()
+
+if __name__ == '__main__':
+    game()
